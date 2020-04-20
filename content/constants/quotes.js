0a2c5ad98554dc481032d8270c9d145f5a90c994
Rodney Payne <paynerg2@gmail.com>
2020-04-20 17:11:08 -0400

    Add cover image

---
+++ content/constants/quotes.js
@@ -0,0 +1,11 @@
+const NightIsShortWalkOnGirl = `"I gazed proudly up at the sky and remembered something Mr. Rihaku had said while 
+we were drinking together, It made me feel so cheerful, I felt I wanted to recite it 
+like a spell to protect me. I murmured it to myself. The night is short - walk on, girl."`
+
+export const quotes = [
+  {
+    quote: NightIsShortWalkOnGirl,
+    attribution: `Her`,
+    source: `The Night is Short, Walk On Girl`,
+  },
+]
---
+++ src/components/coverImage.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { quotes } from "../../content/constants/quotes"
+
+import "./styles.css"
+
+const CoverImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(
+        relativePath: { eq: "jon-cellier-jbeW1fG79HI-unsplash.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 1080) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  const quotation = quotes[0]
+
+  return (
+    <>
+      <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+
+      <div className="quote main">
+        <div>{quotation.quote}</div>
+        <div className="quote attribution">
+          <span
+            style={{ fontStyle: "normal" }}
+          >{`•    ${quotation.attribution}`}</span>
+
+          <span>{`,      ${quotation.source}`}</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CoverImage
