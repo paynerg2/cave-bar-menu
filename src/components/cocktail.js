126837f4880ff28fae887bb0c3f98b8ef67c1ab3
Rodney Payne <paynerg2@gmail.com>
2020-04-20 17:10:51 -0400

    Add cocktail component

---
+++ src/components/cocktail.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import { toStringList } from "../utils/toStringList"
+
+import "./styles.css"
+
+export const Cocktail = ({ node }) => {
+  const ingredientList = toStringList(node.ingredients)
+
+  return node ? (
+    <div className="container">
+      <div className="cocktail-name underline">{node.name}</div>
+      <div className="ingredients">{ingredientList}</div>
+      {node.image && <img src={node.image.fluid.src} alt={node.name} />}
+      {node.description && (
+        <div className="quote">{node.description.internal.content}</div>
+      )}
+    </div>
+  ) : null
+}
