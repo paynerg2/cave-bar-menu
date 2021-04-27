6146c8719f3cc5d0b6e86e2af5b0759c5c4ac40b
Rodney Payne <paynerg2@gmail.com>
2021-04-27 19:40:14 -0400

    Add helper functions for filtering.

---
+++ src/utils/filters.js
@@ -0,0 +1,34 @@
+/**
+ * Filter for string values where the value must match the singular filter.
+ * @param {string} value
+ * @param {string} filter
+ * @returns {boolean}
+ */
+export const exclusiveFilter = (value, filter) => {
+  if (value === null) {
+    return false
+  }
+  if (filter === null) {
+    return true
+  }
+  return value.toLowerCase() === filter
+}
+
+/**
+ * Filter for determining if two arrays share *any* common values and filtering based on that.
+ * @param {array[string]} values
+ * @param {array[string]} filters
+ * @returns {boolean}
+ */
+export const inclusiveFilter = (values, filters) => {
+  // Null checks
+  if (filters === null) {
+    return true
+  }
+  if (values === null) {
+    return false
+  }
+
+  //Check to see if ANY OF the filter values are in the list of ingredients.
+  return values.some(value => filters.includes(value))
+}
