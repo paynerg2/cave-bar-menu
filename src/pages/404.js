29a4d48d770e70972fa5eb22077d0840dd5d5f55
Rodney Payne <paynerg2@gmail.com>
2021-04-28 15:02:03 -0400

    Remove menu from layout on 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout location={location} title={siteTitle} showMenu={false}>
       <SEO title="404: Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
